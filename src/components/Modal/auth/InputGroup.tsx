dc5c5d6d77019ab0f35ccc90aff3f928a834c023
Lucas :) <91512054+queirozlc@users.noreply.github.com>
2023-03-16 19:39:06 -0300

    feat: handling validation
    adding custom input styles based on yup and formik validations

--- src/components/Modal/auth/InputGroup.tsx
+++ src/components/Modal/auth/InputGroup.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+
 interface InputGroupProps {
   label: string
   type: string
@@ -5,6 +7,11 @@ interface InputGroupProps {
   placeholder: string
   required: boolean
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
+  onBlur?: (e: React.FocusEvent<HTMLInputElement>) => void
+  value?: string
+  error?: string
+  isValid?: boolean
+  dirty?: boolean
 }
 
 const InputGroup: React.FC<InputGroupProps> = ({
@@ -13,22 +20,46 @@ const InputGroup: React.FC<InputGroupProps> = ({
   id,
   placeholder,
   required,
-  onChange
+  onChange,
+  value,
+  error,
+  isValid,
+  dirty
 }) => {
   return (
-    <div className="flex items-center relative">
-      <input
-        type={type}
-        id={id}
-        className="peer authInput"
-        placeholder={placeholder}
-        required={required}
-        onChange={onChange}
-      />
-      <label htmlFor={id} className="floatingLabel">
-        {label}
-      </label>
-    </div>
+    <>
+      <div className="flex items-center relative">
+        <input
+          type={type}
+          id={id}
+          className={`peer authInput ${error && 'inputInvalid'}
+          ${!error && dirty && 'inputValid'}`}
+          placeholder={placeholder}
+          required={required}
+          onChange={onChange}
+          value={value}
+        />
+        <label htmlFor={id} className="floatingLabel">
+          {label}
+        </label>
+        {!error && dirty && (
+          <div className="h-3 w-3 absolute right-5">
+            <Image
+              src="/images/verified.svg"
+              alt="Input valid icon"
+              fill
+              className="object-contain"
+            />
+          </div>
+        )}
+      </div>
+
+      {error && (
+        <span className="text-error-500 text-xs font-medium pl-4 !mt-2">
+          {error}
+        </span>
+      )}
+    </>
   )
 }
 
