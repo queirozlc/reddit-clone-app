56c4ea75cfd67398f5c97526a6c37209a8f075fb
Lucas :) <91512054+queirozlc@users.noreply.github.com>
2023-03-16 19:36:22 -0300

    feat: extracting the stylization to global css file

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -16,41 +16,25 @@ interface ButtonProps {
   variant?: IVariant
   onClick?: () => void
   disabled?: boolean
+  type?: 'submit' | 'button'
 }
 
 const Button: React.FC<ButtonProps> = ({
   text,
   variant,
   onClick,
-  disabled
+  disabled,
+  type
 }) => {
-  const base =
-    'rounded-full outline-none transition-all duration-100 ease-in-out'
-
   return (
     <button
-      className={`${base} ${disabled && 'buttonDisabled'}
-      ${
-        variant?.solid &&
-        `bg-link-500 text-white border-none font-body font-bold text-sm
-         hover:bg-link-500/80 w-20 lg:w-28 py-1.5`
-      }
-
-      ${
-        variant?.brand &&
-        `bg-brand-500 text-white border-none font-title font-bold text-sm md:px-5 lg:px-8 py-1.5 hover:bg-brand-500/90}`
-      }
-
-      ${
-        variant?.outline &&
-        `bg-white hidden md:block text-link-500 border border-link-500 md:px-5 lg:px-8 py-1 text-sm
-        hover:bg-link-100/25 font-body
-        font-bold`
-      }
-      ${
-        variant?.oauth &&
-        `relative flex h-9 w-full cursor-pointer items-center justify-center rounded-full border border-gray-200 bg-transparent px-4 font-title font-medium text-sm text-soft-gray-500 outline-none transition-all duration-300 hover:border-blue-200 hover:bg-lighest-blue-500/10`
-      } `}
+      type={type}
+      className={`buttonBase
+      ${disabled && 'buttonDisabled'}
+      ${variant?.solid && `buttonSolid`}
+      ${variant?.brand && 'buttonBrand'}
+      ${variant?.outline && 'buttonOutline'}
+      ${variant?.oauth && 'buttonOAuth'} `}
       onClick={onClick}
     >
       {text}
