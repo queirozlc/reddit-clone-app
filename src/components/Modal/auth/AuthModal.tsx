33b24b76bd62e6e5c6499957d679a7e926fc729a
Lucas :) <91512054+queirozlc@users.noreply.github.com>
2023-03-16 14:58:17 -0300

    feat: modal for authentication

---
+++ src/components/Modal/auth/AuthModal.tsx
@@ -0,0 +1,42 @@
+import { authModalState } from '@/utils/atoms/authModalAtom'
+import { useSetRecoilState } from 'recoil'
+import { useMediaQuery } from 'usehooks-ts'
+import AuthModalBody from './AuthModalBody'
+
+interface AuthModalProps {
+  isOpen: boolean
+}
+
+const AuthModal: React.FC<AuthModalProps> = ({ isOpen }) => {
+  const isMobile = useMediaQuery('(max-width: 768px)')
+  const setAuthModalState = useSetRecoilState(authModalState)
+
+  function handleClose() {
+    setAuthModalState((prev) => ({
+      ...prev,
+      isOpen: false
+    }))
+  }
+
+  return (
+    <>
+      <div className="relative z-10">
+        <div className={`${isOpen ? 'overlay' : 'invisible'}`}>
+          <div
+            className={`fixed inset-0 z-10  transition-all duration-500 transform  ${
+              isOpen ? 'translate-y-0 visible' : 'invisible translate-y-96'
+            }`}
+          >
+            {isOpen && (
+              <div className="flex min-h-full items-end :justify-center md:items-center">
+                <AuthModalBody handleClose={handleClose} isMobile={isMobile} />
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default AuthModal
