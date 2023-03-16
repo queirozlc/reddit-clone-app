b5f2a5e32587fea4837a0b2615f3f7543a94ca9a
Lucas :) <91512054+queirozlc@users.noreply.github.com>
2023-03-16 14:35:57 -0300

    feat: header buttons for login and sign up

---
+++ src/components/Header/RightContent/AuthButtons.tsx
@@ -0,0 +1,32 @@
+import Button from '@/components/Button'
+import { authModalState } from '@/utils/atoms/authModalAtom'
+import { useSetRecoilState } from 'recoil'
+
+const AuthButtons: React.FC = () => {
+  const setAuthModalState = useSetRecoilState(authModalState)
+
+  function handleOpenAuthModal(view: 'login' | 'signup') {
+    setAuthModalState((prev) => ({
+      ...prev,
+      isOpen: true,
+      view
+    }))
+  }
+
+  return (
+    <>
+      <Button
+        variant={{ solid: true }}
+        text="Log In"
+        onClick={() => handleOpenAuthModal('login')}
+      />
+      <Button
+        variant={{ outline: true }}
+        text="Sign Up"
+        onClick={() => handleOpenAuthModal('signup')}
+      />
+    </>
+  )
+}
+
+export default AuthButtons
